dfb412ee43459cfbac90813fdee552af7ffad233
Mark Harwood <msharwood12@gmail.com>
2020-04-29 20:10:04 -0400

    adding now.json

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/testing', (req, res) => {
+    const { name, message } = req.body; 
+    res.send(`${name} wants to tell you ${message}`);
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
