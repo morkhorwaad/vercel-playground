5b18770ebc2162a1280ea410e2e6da93558cf068
Mark Harwood <msharwood12@gmail.com>
2020-04-29 20:31:33 -0400

    adding a post endpoint

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/test', (req, res) => res.json({ "response": "fucku" }));
 app.post('/testing', (req, res) => {
     const { name, message } = req.body; 
     res.send(`${name} wants to tell you ${message}`);
-})
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
